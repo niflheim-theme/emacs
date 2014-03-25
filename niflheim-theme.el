0f285a16b409e5a7ded8fa4dc4c37afc9b2e261e
Nicolas Petton <petton.nicolas@gmail.com>
2014-03-25 20:42:08 +0100

    Fixes autoload section

--- niflheim-theme.el
+++ niflheim-theme.el
@@ -131,11 +131,9 @@
 
 
 ;;;###autoload
-(and load-file-name
-     (boundp 'custom-theme-load-path)
-     (add-to-list 'custom-theme-load-path
-(file-name-as-directory
-(file-name-directory load-file-name))))
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide-theme 'niflheim)
 
